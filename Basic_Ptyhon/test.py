7ec8ed1c7376ae9a57657ac56a9aaf663529a340
Peemai-Techit <366436241005-st@rmutsb.ac.th>
2025-07-18 12:22:25 +0700

    First commit

---
+++ Basic_Ptyhon/test.py
@@ -0,0 +1,11 @@
+# create dictionaries key : value
+group_idm = {
+    1 : "film",
+    2 : "tam",
+    3 : "kade",
+    4 : "nice",
+    5 : "mai"
+}
+group_idm[6] = "tum" # Add elememt to  dictionaries
+# print(group_idm)
+print(group_idm[4]) # get value if key 
